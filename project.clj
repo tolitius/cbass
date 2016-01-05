8d102f5f5583f107e1286d287b1c1121feb5b96a
anatoly <tolitius@gmail.com>
2016-01-05 15:38:59 -0500

    releasing 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cbass "0.1.4-SNAPSHOT"
+(defproject cbass "0.1.5-SNAPSHOT"
   :description "adding simple to HBase"
   :url "https://github.com/tolitius/cbass"
   :license {:name "Eclipse Public License"
