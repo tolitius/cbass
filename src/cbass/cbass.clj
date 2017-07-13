407fe14cf50488c883b14e5d0ec370b26f884e39
Rick Mangi <rick@chartbeat.com>
2017-07-13 17:30:48 -0400

    Use hbase 1.2.6 client lib

--- src/cbass/cbass.clj
+++ src/cbass/cbass.clj
@@ -6,7 +6,7 @@
            [org.apache.hadoop.hbase.util Bytes]
            [org.apache.hadoop.hbase TableName]
            [org.apache.hadoop.conf Configuration]
-           [org.apache.hadoop.hbase.client HConnection HConnectionManager HTableInterface Get Put Delete Scan Result]))
+           [org.apache.hadoop.hbase.client Connection ConnectionFactory Table Get Put Delete Scan Result]))
 
 (def pack (atom n/freeze))
 (def unpack (atom thaw))
@@ -15,15 +15,15 @@
   (when p (reset! pack p))
   (when u (reset! unpack u)))
 
-(defn ^HConnection new-connection [conf & {:keys [pack unpack]}]
+(defn ^Connection new-connection [conf & {:keys [pack unpack]}]
   (let [configuration (Configuration.)]
     (doseq [[k v] conf]
       (.set configuration k (str v)))
     (pack-un-pack {:p pack :u unpack})
-    (HConnectionManager/createConnection configuration)))
+    (ConnectionFactory/createConnection configuration)))
 
-(defn get-table [^HConnection c ^String t-name]
-  (.getTable c t-name))
+(defn get-table [^Connection c ^String t-name]
+  (.getTable c (TableName/valueOf t-name)))
 
 (defn result-key [rk]
   (-> (String. (key rk)) keyword))
@@ -62,7 +62,7 @@
     [k (dissoc v :last-updated)]))
 
 (defn scan [conn table & {:keys [row-key-fn limit with-ts? lazy?] :as criteria}]
-  (with-open [^HTableInterface h-table (get-table conn table)]
+  (with-open [^Table h-table (get-table conn table)]
     (let [results (-> (.iterator (.getScanner h-table (scan-filter criteria)))
                       iterator-seq)
           row-key-fn (or row-key-fn #(String. %))
@@ -80,7 +80,7 @@
   ([conn table row-key family]
    (find-by conn table row-key family nil))
   ([conn table row-key family columns]
-    (with-open [^HTableInterface h-table (get-table conn table)]
+    (with-open [^Table h-table (get-table conn table)]
       (let [^Get g (Get. (to-bytes row-key))]
         (when family
           (if columns
@@ -92,13 +92,13 @@
 
 (defn store
   ([conn table row-key family]
-    (with-open [^HTableInterface h-table (get-table conn table)]
+    (with-open [^Table h-table (get-table conn table)]
       (let [^Put p (Put. (to-bytes row-key))]
         (.put h-table (.add p (to-bytes family)   ;; in case there are no columns, just store row-key and family
                               no-values
                               no-values)))))
   ([conn table row-key family columns]
-    (with-open [^HTableInterface h-table (get-table conn table)]
+    (with-open [^Table h-table (get-table conn table)]
       (let [^Put h-data (map->hdata row-key family columns)]
         (.put h-table h-data)))))
 
@@ -110,7 +110,7 @@
     p))
 
 (defn store-batch [conn table rows]
-  (with-open [^HTableInterface h-table (get-table conn table)]
+  (with-open [^Table h-table (get-table conn table)]
     (let [bulk (ArrayList. (for [[r f cs] rows]
                              (if cs
                                (map->hdata r f cs)
@@ -124,7 +124,7 @@
   ([conn table row-key family]
    (delete conn table row-key family nil))
   ([conn table row-key family columns]
-    (with-open [^HTableInterface h-table (get-table conn table)]
+    (with-open [^Table h-table (get-table conn table)]
       (let [^Delete d (Delete. (to-bytes row-key))]
         (when family
           (if columns
@@ -144,5 +144,5 @@
                                           (delete-key-fn %)
                                           (to-bytes %))) row-keys))]
     (when (seq row-keys)
-      (with-open [^HTableInterface h-table (get-table conn table)]
+      (with-open [^Table h-table (get-table conn table)]
         (.delete h-table bulk)))))
